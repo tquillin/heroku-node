607d2f4bd268537c009b285a1e81185163bdd635
Terrell Quillin <terrellq@gmail.com>
2016-02-17 19:00:51 -0700

    Adds new files.

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://localhost/products_api');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var indexRouter = require('./server/routes/index');
+var productsRouter = require('./server/routes/api/products');
+
+app.get('/inventory', function(req, res){
+  res.render('views/inventory');
+});
+
+app.use('/inventory', productsRouter);
+
+
+
+app.use('/', indexRouter);
+app.use('/api/products', productsRouter);
+
+var port = 8080;
+app.listen(port, function(){
+  console.log("je t'écoute..." + port);
+});
+
+
+
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+// app.get('/', function(req, res) {
+//
+//     // ejs render automatically looks in the views folder
+//     res.render('index');
+// });
+//
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
